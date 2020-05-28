b338347ad1e5e5ed7e1272e0a290604f6209eb8a
Christopher Oehring <christopher.oehring@tu-ilmenau.de>
2020-05-29 00:18:58 +0200

    fixup! added permission methods

--- group-manager/src/main/java/com/studilieferservice/groupmanager/service/GroupService.java
+++ group-manager/src/main/java/com/studilieferservice/groupmanager/service/GroupService.java
@@ -8,6 +8,7 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static com.studilieferservice.groupmanager.service.GroupEventType.*;
