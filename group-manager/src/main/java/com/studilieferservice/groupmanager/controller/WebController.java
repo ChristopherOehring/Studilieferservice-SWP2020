7a3a44be6e3bc43c3694352ccad088fa62257477
ChristopherOehring <chris.oehring@gmx.de>
2020-06-09 06:05:20 +0200

    You can now create Groups via the userMenu page

--- group-manager/src/main/java/com/studilieferservice/groupmanager/controller/WebController.java
+++ group-manager/src/main/java/com/studilieferservice/groupmanager/controller/WebController.java
@@ -1,17 +1,18 @@
 package com.studilieferservice.groupmanager.controller;
 
 import com.studilieferservice.groupmanager.controller.bodys.CreationForm;
+import com.studilieferservice.groupmanager.controller.bodys.GroupAndUserBody;
 import com.studilieferservice.groupmanager.persistence.Gruppe;
+import com.studilieferservice.groupmanager.persistence.Invite;
 import com.studilieferservice.groupmanager.persistence.User;
 import com.studilieferservice.groupmanager.service.GroupService;
+import com.studilieferservice.groupmanager.service.InviteService;
 import com.studilieferservice.groupmanager.service.UserService;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -34,15 +35,17 @@ public class WebController {
     private final GroupService groupService;
     private final UserService userService;
     private String groupManagerSrc;
-
+    private InviteService inviteService;
 
     @Autowired
-    public WebController(GroupService groupService,
+    public WebController(final @Value("${group-manager-src}") String groupManagerSrc,
+                         GroupService groupService,
                          UserService userService,
-                         final @Value("${group-manager-src}") String groupManagerSrc) {
+                         InviteService inviteService) {
         this.groupService = groupService;
         this.userService = userService;
         this.groupManagerSrc = groupManagerSrc;
+        this.inviteService = inviteService;
     }
 
     /**
@@ -80,6 +83,12 @@ public class WebController {
         return "userMenu";
     }
 
+    @GetMapping("/groupMenu/{groupId}")
+    public String groupMenu(Model model, @PathVariable("groupId") String groupId) {
+        model.addAttribute("groupId", groupId);
+
+        return "groupMenu";
+    }
 
     /**
      * Meant to be used by a webpage to create a new group. Will redirect to the index afterwards.
@@ -91,23 +100,19 @@ public class WebController {
      */
     @PostMapping("/save-group")
     public String saveGroupSubmission(@ModelAttribute CreationForm form) throws JSONException {
-        Gruppe gruppe = new Gruppe();
-        gruppe.setGroupName(form.getGroupName());
-        gruppe.setId(UUID.randomUUID().toString());
+        Gruppe group = new Gruppe();
+        group.setGroupName(form.getGroupName());
+        group.setId(UUID.randomUUID().toString());
 
-        String[] users = form.getUser()
-                .replace(" ", "")
-                .split(",");
+        Optional<User> optionalUser = userService.findById(form.getUser());
+        if(optionalUser.isEmpty()) System.out.println("No user with email "+form.getUser()+" was found!");
+        User user = optionalUser.get();
 
-        //TODO fixen ^^ -> firstname/lastname have to be replaced later on, also it might not be the best idea just to add a new user without saving in the user-repository
-        for(String s: users){
-            User u = new User(s, "fistname", "lastname", "username");
-            gruppe.addMember(u);
-            userService.save(u);
-        }
+        group.setOwner(user);
 
-        groupService.save(gruppe);
+        groupService.save(group);
 
+        /*
         //following code is for telling the shopping-list-manager via POST to create a shopping list for the new group
         String urlPOST = "http://localhost:8070/shoppingList/create";
 
@@ -123,8 +128,9 @@ public class WebController {
 
         RestTemplate restTemplate = new RestTemplate();
         restTemplate.postForObject(urlPOST, request , String.class );
+        */
 
-        return "redirect:index";
+        return "redirect:groupMenu/" + group.getId();
     }
 
     /**
@@ -172,18 +178,52 @@ public class WebController {
         groupService.save(group);
 
 //TODO: and where do we go afterwards?
-        return "redirect:groupCreator";
+        return "redirect:groupMenu/" + group.getId();
     }
 
-    @GetMapping("/groupUsers/{groupId}")
-    public String getMembers(Model model, @PathVariable("groupId") String groupId) {
-        Optional<Gruppe> gruppeOptional = groupService.findById(groupId);
-        if (gruppeOptional.isEmpty()) return "Error: group could not be found";
-        Gruppe group = gruppeOptional.get();
+    @PostMapping("/invite")
+    public String addInvite(@RequestBody GroupAndUserBody body) {
+        Optional<User> optionalUser = userService.findById(body.getEmail());
+        if(optionalUser.isEmpty()) System.out.println("No user with email "+body.getEmail()+" was found!");
+        User user = optionalUser.get();
 
-        model.addAttribute("owner", group.getOwner());
-        model.addAttribute("admins", group.getAdmins());
-        model.addAttribute("members", group.getMembers());
-        return "groupUsers";
+        Optional<Gruppe> optionalGruppe = groupService.findById(body.getGroupId());
+        if (optionalGruppe.isEmpty()) System.out.println("No group with id "+body.getGroupId()+" was found!");
+        Gruppe gruppe = optionalGruppe.get();
+
+        inviteService.addInvite(new Invite(gruppe, user));
+
+        return "redirect:groupMenu";//fixme
+    }
+
+    @PutMapping("/acceptInvite")
+    public String acceptInvite(@RequestBody GroupAndUserBody body) {
+        Optional<User> optionalUser = userService.findById(body.getEmail());
+        if(optionalUser.isEmpty()) System.out.println("No user with email "+body.getEmail()+" was found!");
+        User user = optionalUser.get();
+
+        Optional<Gruppe> optionalGruppe = groupService.findById(body.getGroupId());
+        if (optionalGruppe.isEmpty()) System.out.println("No group with id "+body.getGroupId()+" was found!");
+        Gruppe gruppe = optionalGruppe.get();
+
+        inviteService.removeInvite(new Invite(gruppe, user));
+        gruppe.addMember(user);
+
+        return "redirect:userMenu";
+    }
+
+    @PutMapping("/declineInvite")
+    public String declineInvite(@RequestBody GroupAndUserBody body) {
+        Optional<User> optionalUser = userService.findById(body.getEmail());
+        if(optionalUser.isEmpty()) System.out.println("No user with email "+body.getEmail()+" was found!");
+        User user = optionalUser.get();
+
+        Optional<Gruppe> optionalGruppe = groupService.findById(body.getGroupId());
+        if (optionalGruppe.isEmpty()) System.out.println("No group with id "+body.getGroupId()+" was found!");
+        Gruppe gruppe = optionalGruppe.get();
+
+        inviteService.removeInvite(new Invite(gruppe, user));
+
+        return "redirect:userMenu";
     }
 }
